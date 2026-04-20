83b0d09d8db65880f98f1043a47faf075416d49c
AidenChen <wei310129@gmail.com>
2026-04-20 09:38:16 +0800

    feat: initialize Spring Boot application with Maven wrapper and basic configuration

---
+++ src/main/java/tw/com/aidenmade/jfrjmc/JfrJmcApplication.java
@@ -0,0 +1,13 @@
+package tw.com.aidenmade.jfrjmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfrJmcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JfrJmcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tw/com/aidenmade/jfrjmc/JfrJmcApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.aidenmade.jfrjmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfrJmcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
